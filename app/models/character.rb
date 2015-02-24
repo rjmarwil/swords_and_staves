c63b547379fbce735d92c3763645efbe4a022cbe
Robert Marwil <robertmarwil@Roberts-MacBook-Air.local>
2015-02-24 15:29:24 -0700

    change character to only belong_to character classes

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,7 +1,7 @@
 class Character < ActiveRecord::Base
 
-  has_and_belongs_to_many :character_classes
   has_and_belongs_to_many :items
+  belongs_to :character_class
   belongs_to :party
   belongs_to :location
   has_one :mount
